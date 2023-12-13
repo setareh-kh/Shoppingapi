8f7693dd1087dbccf61be60765e95ed0a3eaf576
DESKTOP-NBKS71J\Star <setareh.hoh@gmail.com>
2023-12-13 07:09:34 +0330

    tip tip tip...creat maps in mapperprofile pleaseeeeeeeeee

--- MapperProfile.cs
+++ MapperProfile.cs
@@ -10,6 +10,8 @@ namespace Shoppingapi.Mapper
         {
             //mapping requests dtos to entity class
             CreateMap<StoreUserDto,User>();
+            CreateMap<UpdateUserDto,User>();
+
         }
     }
 }
