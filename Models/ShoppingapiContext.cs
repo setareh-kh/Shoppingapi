672a3479aa6f8325207aed2bf88b1f03bb62cdd9
DESKTOP-NBKS71J\Star <setareh.hoh@gmail.com>
2023-12-30 15:30:47 +0330

    catogory repo

--- Models/ShoppingapiContext.cs
+++ Models/ShoppingapiContext.cs
@@ -10,6 +10,6 @@ namespace Shoppingapi.Models
         }
         public DbSet<User> Users {get; set;}
         public DbSet<Product> Products {get; set;}
-        public DbSet<CatogoryProduct>   {get; set;}
+        public DbSet<CatogoryProduct> CatogoryProducts {get; set;}
     }
 }
---
+++ Repositories/Repositories/CatogoryProductRepository.cs
@@ -0,0 +1,50 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Shoppingapi.Dtos.Requests;
+using Shoppingapi.Models;
+
+namespace Shoppingapi.Repositories.Repositories
+{
+    public class CatogoryProductRepository
+    {
+
+        private readonly ShoppingapiContext _context;
+        private IMapper _mapper;
+
+        public CatogoryProductRepository(ShoppingapiContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<CatogoryProduct> StoreAsync(StoreCatogoryDto storeCatogoryDto)
+        {
+            CatogoryProduct newCatogory = _mapper.Map<CatogoryProduct>(storeCatogoryDto);
+            await _context.CatogoryProducts.AddAsync(newCatogory);
+            return newCatogory;
+        }
+        public async Task<CatogoryProduct?> GetAsync(int id)
+        {
+            CatogoryProduct? catogory = await _context.CatogoryProducts.FindAsync(id);
+            return catogory;
+
+        }
+        public async Task<List<CatogoryProduct>?> GetAllAsync()
+        {
+            List<CatogoryProduct> catogoryProducts = await _context.CatogoryProducts.ToListAsync();
+            return catogoryProducts;
+        }
+        public async Task<bool> DeleteAsync(int id)
+        {
+            CatogoryProduct? catogory = await _context.CatogoryProducts.FindAsync(id);
+            if (catogory != null)
+            {
+                _context.CatogoryProducts.Remove(catogory);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+
+
+        }
+    }
+}
